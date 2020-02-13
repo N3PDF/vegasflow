88a0993fe953c529f12568b16f5cc5f84c849080
Scarlehoff <jumax9.gently@gmail.com>
2020-02-13 21:48:14 +0000

    change benchmark nevents and limits in scritp

--- benchmark/run_example.sh
+++ benchmark/run_example.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-example=drellyan_lo_tf.py
-nevents=$(( 10**8 ))
-limit=$(( 10**6 ))
+# example=drellyan_lo_tf.py
+example=lepage_tf.py
+nevents=$(( 4*10**8 ))
+limit=$(( 10**8 ))
 
 
 # Run CPU times:
@@ -19,12 +20,12 @@ echo "### GPU benchmark ###"
 echo " CPU limited to 0-4 "
 echo " > > Running on Titan V"
 export CUDA_VISIBLE_DEVICES=0
-taskset -c 0-4 python ${example} -n ${nevents} -l ${limit} -q 2>/dev/null
+taskset -c 0-3 python ${example} -n ${nevents} -l ${limit} -q 2>/dev/null
 
 echo " > > Running on RTX 2080 Ti"
 export CUDA_VISIBLE_DEVICES=1
-taskset -c 0-4 python ${example} -n ${nevents} -l ${limit} -q 2>/dev/null
+taskset -c 0-3 python ${example} -n ${nevents} -l ${limit} -q 2>/dev/null
 
 echo " > > Running on Titan V and RTX 2080 Ti"
 export CUDA_VISIBLE_DEVICES=0,1
-taskset -c 0-4 python ${example} -n ${nevents} -l ${limit} -q 2>/dev/null
+taskset -c 0-7 python ${example} -n ${nevents} -l ${limit} -q 2>/dev/null
